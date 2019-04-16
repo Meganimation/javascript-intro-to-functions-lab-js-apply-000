f33fdc940d8cdb3be76c2d383411e3956d6f308c
Megan Adams <meganoliviawendyadams@gmail.com>
2019-04-16 05:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO";
   var lowercase = "hello";
   
-  if (Shout.toUpperCase("HELLO") === true 
+  if (logShout.toUpperCase(string) === true 
   )
   return ("I can't hear you!");
   }
